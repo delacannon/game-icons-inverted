4986b2ef1e235ec66d0934f9879585708863e4cb
Delacannon <delacannon@gmail.com>
2020-09-02 11:22:04 +0200

    updated icons

--- get-icons.sh
+++ get-icons.sh
@@ -6,8 +6,8 @@ filename="game-icons.net.svg"
 url="https://game-icons.net/archives/svg/zip/000000/transparent/game-icons.net.svg.zip"
 
 if curl --silent -o "${PWD}/${filename}" -L "${url}"; then
-    unzip "${filename}"
-    # move up 
+    unzip "${filename}" 'icons/000000/transparent/1x1/*' -d all-icons
+    # move up and put all icons into the all-icons folder
     for i in */; do find "$i" -type f -exec mv {} "$i" \; && rm -r "$i"*/; done
 else
     echo "Something went wrong"
