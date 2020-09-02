19639f74055a561cc830138629c0a0e5980a4c96
Delacannon <delacannon@gmail.com>
2020-09-02 11:19:28 +0200

    updated icons

--- get-icons.sh
+++ get-icons.sh
@@ -7,6 +7,8 @@ url="https://game-icons.net/archives/svg/zip/000000/transparent/game-icons.net.s
 
 if curl --silent -o "${PWD}/${filename}" -L "${url}"; then
     unzip "${filename}"
+    # move up 
+    for i in */; do find "$i" -type f -exec mv {} "$i" \; && rm -r "$i"*/; done
 else
     echo "Something went wrong"
 fi
